a2d7834a2095a0c38f9ee03e51a66540c112ed25
Nabil19M <115401006+Nabil19M@users.noreply.github.com>
2024-01-11 23:48:01 +0200

    Add files via upload

---
+++ test.c
@@ -0,0 +1,50 @@
+#include "lib.h"
+
+/**
+ * main - entry point for a simple shell program
+ * @ac: the number of arguments (unused)
+ * @av: an array of strings containing the program arguments
+ * Return: the exit status of the program
+ */
+int main(int ac __attribute__((unused)), char *av[])
+{
+	char *buffer;
+	size_t bufsize = 0;
+	int cnt = 1;
+	int r = EXIT_SUCCESS;
+	pid_t procid;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(1, "$", 1);
+		if (getline(&buffer, &bufsize, stdin) == EOF)
+		{
+			fflush(stdin);
+			break;
+		}
+	buffer[_strcspn(buffer, "\n")] = 0;
+	if (_strcmp(buffer, "exit") == 0)
+		break;
+
+	procid = fork();
+
+	if (procid == -1)
+		exit(EXIT_FAILURE);
+
+	else if (procid == 0)
+	{
+		if (_strcmp(buffer, "env") == 0)
+		{
+			print_env();
+		}
+
+		execute_command(buffer, av, cnt);
+	}
+	else
+		wait(NULL);
+	fflush(stdin);
+	cnt++;
+	}
+	return (r);
+}
